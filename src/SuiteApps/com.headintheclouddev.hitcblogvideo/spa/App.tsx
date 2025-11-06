215e5c390388eac260cc297520664abe74c24fc8
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2025-11-06 13:57:11 +0000

    Blog Placeholders

--- src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/App.tsx
+++ src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/App.tsx
@@ -1,13 +1,14 @@
-import {ContentPanel, Heading} from '@uif-js/component';
+import {Heading} from '@uif-js/component';
 import {JSX} from '@uif-js/core';
 
 export default function App(): JSX.Element {
   return (
-    <ContentPanel
-      horizontalAlignment={ContentPanel.HorizontalAlignment.CENTER}
-      verticalAlignment={ContentPanel.VerticalAlignment.CENTER}
-    >
-      <Heading>Hello World!</Heading>
-    </ContentPanel>
+    <div>
+      <Heading>NetSuite Blog</Heading>
+      <Heading>Theme control</Heading>
+      <Heading>User bar - login and registration</Heading>
+      <Heading>(When logged in) Create New Post</Heading>
+      <Heading>Post List</Heading>
+    </div>
   );
 }
