8e472f30cf80528a0d0c8b8db67ea47f8cbde8d3
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2025-11-11 14:41:09 +0000

    PostList: Moved dummy data to actions

--- src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/components/PostList.tsx
+++ src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/components/PostList.tsx
@@ -2,10 +2,10 @@ import { JSX, VDom } from "@uif-js/core";
 import Post, {IPost} from "./Post";
 
 export default function PostList() {
-  const postsJSON: IPost[] = [ // TODO: Get this from the app state
-    { author: 'Robbie', title: 'Test Post 1', content: 'It is a beautiful day to be a NetSuite developer!' },
-    { author: 'Robbie', title: 'Test Post 2', content: 'Looking forward to SuiteWorld 2026.' }
-  ];
+  // const postsJSON: IPost[] = [ // TODO: Get this from the app state
+  //   { author: 'Robbie', title: 'Test Post 1', content: 'It is a beautiful day to be a NetSuite developer!' },
+  //   { author: 'Robbie', title: 'Test Post 2', content: 'Looking forward to SuiteWorld 2026.' }
+  // ];
   const postsList: JSX.Element[] = [];
   for (const post of postsJSON) {
     postsList.push(
