52c0acd3db926d0275d64485030130f5549bb624
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2025-11-11 14:47:39 +0000

    App: State Management

--- src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/App.tsx
+++ src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/App.tsx
@@ -1,11 +1,27 @@
 import {Heading} from '@uif-js/component';
-import {JSX, VDom, useState} from '@uif-js/core';
+import {JSX, Store, VDom, useMemo, useState, useEffect} from '@uif-js/core';
 import PostList from "./components/PostList";
 import {ThemeContext} from "./contexts";
 import ChangeTheme from "./components/ChangeTheme";
 import UserBar from "./components/UserBar";
+import {IPost} from "./components/Post";
+import appReducer from "./reducers";
 
 export default function App(): JSX.Element {
+  const initialState: IAppState = { user: '', posts: [], error: '' };
+  const [state, setState] = useState(initialState);
+  const store = useMemo(() => {
+    return Store.create({
+      reducer: appReducer,
+      state,
+      onStateChanged: ({ currentState }) => setState(currentState),
+    });
+  });
+
+  useEffect(() => { // Keeps the state hook in sync with the redux store state
+    return store.subscribe(() => setState(store.getState()));
+  }, []);
+
   const [theme, setTheme] = useState({ primaryColor: 'deepskyblue', secondaryColor: 'coral' });
   const context = {
     // [StateContext]: { state, dispatch }, // This doesn't work
@@ -22,4 +38,10 @@ export default function App(): JSX.Element {
       </div>
     </VDom.Context>
   );
+}
+
+export interface IAppState {
+  user: string;
+  posts: IPost[];
+  error: string;
 }
