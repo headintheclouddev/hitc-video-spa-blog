a351a53ace85931320c9afa9a20b426928cfb7d7
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2025-11-11 14:59:15 +0000

    Post List: Use empty post array for now

--- src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/components/PostList.tsx
+++ src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/components/PostList.tsx
@@ -2,10 +2,11 @@ import { JSX, VDom } from "@uif-js/core";
 import Post, {IPost} from "./Post";
 
 export default function PostList() {
-  // const postsJSON: IPost[] = [ // TODO: Get this from the app state
+  // const postsJSON: IPost[] = [
   //   { author: 'Robbie', title: 'Test Post 1', content: 'It is a beautiful day to be a NetSuite developer!' },
   //   { author: 'Robbie', title: 'Test Post 2', content: 'Looking forward to SuiteWorld 2026.' }
   // ];
+  const postsJSON: IPost[] = []; // TODO: Get this from the app state
   const postsList: JSX.Element[] = [];
   for (const post of postsJSON) {
     postsList.push(
