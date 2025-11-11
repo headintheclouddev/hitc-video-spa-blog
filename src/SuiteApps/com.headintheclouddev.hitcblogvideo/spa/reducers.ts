330f953db6769ab2e58313d5928a85a2421b63ff
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2025-11-11 14:59:38 +0000

    Reducers: Started filling this out

--- src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/reducers.ts
+++ src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/reducers.ts
@@ -1,5 +1,29 @@
+import {ActionType} from './actions';
+import {IPost} from './components/Post';
+import {IAppState} from "./App";
+
 // Reducers take a state, apply an action, and return a new state
 
-export default function appReducer(state: any, action: any) {
-  return { ...state }; // We're not changing it, we're returning a new object composed of the old state
+export default function appReducer(state: IAppState, action: any): IAppState {
+  return {
+    user: userReducer(state.user, action),
+    posts: postsReducer(state.posts, action),
+    error: '' // TODO: ?
+  }
+}
+
+function userReducer(state: string, action: { type: Symbol, user: string}) { // Here, state is the current user and action is Register, Login, or Logout
+  if (action.type == ActionType.USER_LOGIN) {
+
+  } else if (action.type == ActionType.USER_REGISTER) {
+
+  } else if (action.type == ActionType.USER_LOGOUT) {
+
+  } else {
+    return state;
+  }
+}
+
+function postsReducer(state: IPost[], action: { type: Symbol,  }) { // Here, posts is the current set of posts, and the action is create or fetch
+  return state;
 }
