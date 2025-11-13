6f56edc01fdea99b4bbe0e4cbf9efc2f2edd4053
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2025-11-13 10:47:27 +0000

    Create Post: Placeholders

--- src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/components/CreatePost.tsx
+++ src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/components/CreatePost.tsx
@@ -1,3 +1,20 @@
+import {useSelector} from "@uif-js/core";
+import {IAppState} from "../App";
+import {TextArea, TextBox} from "@uif-js/component";
+
 export default function CreatePost() {
-  return <p>Please log in to create a post.</p>;
+  const user = useSelector((state: IAppState) => state.user);
+  if (!user) return <p>Please log in to create a post.</p>;
+  // Otherwise user is logged in, so they can create a post:
+  return (
+    <div>
+      <div>Author: <b>{user}</b></div>
+      <div>
+        <label htmlFor="create-title">Title:</label>
+        <TextBox type={TextBox.Type.TEXT} name="create-title" />
+      </div>
+      <TextArea />
+      <input type="button" value="Create" />
+    </div>
+  )
 }
