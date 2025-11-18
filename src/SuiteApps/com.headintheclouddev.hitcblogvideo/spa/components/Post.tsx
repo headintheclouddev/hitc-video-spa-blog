9817f84b796e0d40f00466ea6377daac0e127ee2
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2025-11-18 16:42:16 +0000

    Post component: 1. Implementing link to full post 2. Add ID to interface

--- src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/components/Post.tsx
+++ src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/components/Post.tsx
@@ -1,12 +1,16 @@
 import {ThemeContext} from "../contexts";
 import { useContext } from '@uif-js/core';
+import {Link} from "@uif-js/component";
 
 export default function Post(props: IPost) {
   const { secondaryColor } = useContext(ThemeContext);
+  let processedContent = props.content;
+  if (props.content.length > 30) processedContent = props.content.substring(0, 30) + '...';
   return (
     <div>
       <h3 style={{ color: secondaryColor }}>{props.title}</h3>
-      <div>{props.content}</div>
+      <div>{processedContent}</div>
+      <div><br/><Link route={{ route: '/view/:id', parameters: { id: props.id } }}>View full post</Link></div>
       <br />
       <i>Written by <b>{props.author}</b></i>
     </div>
@@ -14,6 +18,7 @@ export default function Post(props: IPost) {
 }
 
 export interface IPost {
+  id: number;
   title: string;
   content: string;
   author: string;
