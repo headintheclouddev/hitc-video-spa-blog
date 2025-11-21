42444dd2803bc7236e286b507d53ab9999a683c4
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2025-11-21 11:18:44 +0000

    Register component: Finished this

--- src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/components/UserRegister.tsx
+++ src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/components/UserRegister.tsx
@@ -1,19 +1,40 @@
-import {TextBox} from "@uif-js/component";
+import {Button, TextBox} from "@uif-js/component";
+import {useDispatch, useState} from "@uif-js/core";
+import {Action} from "../actions";
 
 export default function Register() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [password2, setPassword2] = useState('');
+  const dispatch = useDispatch();
+
+  const eventHandlers = {
+    [TextBox.Event.TEXT_CHANGED]: ({text}) => {
+      console.log('Login username changed to', text); // Runs on every keystroke
+      setUsername(text);
+    }
+  }
+  function handlePassword(evt: TextBox.TextChangedArgs) {
+    setPassword(evt.text);
+  }
+  function handlePassword2(evt: TextBox.TextChangedArgs) {
+    setPassword2(evt.text);
+  }
+
   function handleRegister() {
     console.log('handleRegister running');
+    dispatch(Action.userRegister(username, password));
   }
 
   return (
     <div>
       <label htmlFor="register-username">Username:</label>
-      <TextBox type={TextBox.Type.TEXT} name="register-username" />
+      <TextBox type={TextBox.Type.TEXT} name="register-username" text={username} on={eventHandlers} />
       <label htmlFor="register-password">Password:</label>
-      <TextBox type={TextBox.Type.PASSWORD} name="register-password" />
+      <TextBox type={TextBox.Type.PASSWORD} name="register-password" text={password} onTextChanged={handlePassword} />
       <label htmlFor="register-repeat-password">Repeat Password:</label>
-      <TextBox type={TextBox.Type.PASSWORD} name="register-reeat-password" />
-      <input type="button" value="Register" onClick={handleRegister} />
+      <TextBox type={TextBox.Type.PASSWORD} name="register-reeat-password" text={password2} onTextChanged={handlePassword2} />
+      <Button label="Register" action={handleRegister} enabled={username.length > 0 && password.length > 0 && password == password2} />
     </div>
   )
 }
