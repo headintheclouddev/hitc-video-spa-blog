d33afae3256ea6a7003ed589239fb2b82b2d1032
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2025-11-27 13:40:05 +0000

    Reducers: Implement error reducer

--- src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/reducers.ts
+++ src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/reducers.ts
@@ -8,12 +8,12 @@ export default function appReducer(state: IAppState, action: any): IAppState {
   return {
     user: userReducer(state.user, action),
     posts: postsReducer(state.posts, action),
-    error: '' // TODO: Do we really need this??
+    error: errorReducer(state.error, action)
   }
 }
 
 // Here, state is the currently logged-in user and action is Register, Login, or Logout, along with the new username
-function userReducer(state: string, action: { type: Symbol, username: string }): string {
+function userReducer(state: string, action: { type: Symbol, username: string, error?: string }): string {
   switch (action.type) {
     case ActionType.USER_LOGIN: // User will have been authenticated by the action function, here we're just returning the user for the app state
       return action.username;
@@ -33,6 +33,14 @@ function postsReducer(state: IPost[], action: { type: Symbol, title?: string, co
     return [newPost, ...state];
   } else if (action.type == ActionType.FETCH_POSTS) {
     return action.posts;
+  } else {
+    return state;
+  }
+}
+
+function errorReducer(state: string, action: { type: Symbol, error: string }) {
+  if (action.type == ActionType.LOGIN_ERROR) {
+    return action.error;
   } else {
     return state;
   }
