e47615260850a9f8fd69e24ab57bc22d648ba32f
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2025-11-27 13:49:32 +0000

    Post component: Only shorten the text when in list (short) mode

--- src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/components/Post.tsx
+++ src/SuiteApps/com.headintheclouddev.hitcblogvideo/spa/components/Post.tsx
@@ -1,16 +1,18 @@
 import {ThemeContext} from "../contexts";
-import { useContext } from '@uif-js/core';
+import {JSX, useContext} from '@uif-js/core';
 import {Link} from "@uif-js/component";
 
 export default function Post(props: IPost) {
   const { secondaryColor } = useContext(ThemeContext);
   let processedContent = props.content;
-  if (props.content.length > 30) processedContent = props.content.substring(0, 30) + '...';
+  if (props.short && props.content.length > 30) processedContent = props.content.substring(0, 30) + '...';
+  let linkToFullContent: JSX.Element = null;
+  if (props.short) linkToFullContent = <div><br/><Link route={{ route: '/view/:id', parameters: { id: props.id } }}>View full post</Link></div>
   return (
     <div>
       <h3 style={{ color: secondaryColor }}>{props.title}</h3>
       <div>{processedContent}</div>
-      <div><br/><Link route={{ route: '/view/:id', parameters: { id: props.id } }}>View full post</Link></div>
+      {linkToFullContent}
       <br />
       <i>Written by <b>{props.author}</b></i>
     </div>
@@ -22,4 +24,5 @@ export interface IPost {
   title: string;
   content: string;
   author: string;
+  short?: boolean;
 }
